996cd1690261ece5ffd1f170526331822ac98843
Jocke45 <jocke45@gmail.com>
2020-06-07 13:15:27 +0200

    Started working on booking command

--- booking.py
+++ booking.py
@@ -5,12 +5,20 @@ from beautifultable import BeautifulTable
 import yaml
 import datetime
 
-# Variables
+# TODO
+# book command
+# - write to yaml
+# - check if the store is currently booked and warn/deny
+# - sub parse name argument?
+#
+# Make tables prettier
 
 parser = ArgumentParser(
     prog='Booking', usage='%(prog)s [-help] [--list] [--list STORE_NAME] ', description='A program for listing and booking the stores in the lab')
 parser.add_argument("-l", "--list", nargs="?", default='not_specified', type=str,
                     help="List the whole booking table or only for a store")
+parser.add_argument("-b", "--book", nargs=4, type=str,
+                    help="Book a specified store to a name")
 parser.add_argument("-v", "--verbose", action="store_true",
                     help="increase output verbosity")
 parser.add_argument
@@ -18,10 +26,16 @@ parser.add_argument
 args = parser.parse_args()
 
 # Convert input to upper case if it exists
+# List input
 try:
-    store = args.list.upper()
+    list_store = args.list.upper()
 except AttributeError as identifier:
-    store = False
+    list_store = False
+# Book input
+try:
+    book_store = args.book[0].upper()
+except TypeError as identifier:
+    book_store = False
 
 
 # Functions
@@ -52,21 +66,40 @@ if args.verbose:
     print('')
     print('args.list is of type: ' + str(type(args.list)))
     print('')
+    print('args.book is of type: ' + str(type(args.book)))
+    print('')
     print('Store list is of type: ' + str(type(Store_list)))
     print('')
     print('Stores available:')
     for store in Store_list:
         print(Store_list[store])
     print('')
-    print('Specified store is: ' + str(store))
+    print('Specified list_store is: ' + str(list_store))
+    print('')
+    print('Specified book_store is: ' + str(book_store))
     print('')
 
 
 if args.list != 'not_specified':
-    if not store:
+    if not list_store:
         createTable(Store_list, 1)
     else:
-        if store in Store_list:
+        if list_store in Store_list:
             createTable(Store_list[store], 0)
+        else:
+            print('No store matching that name was found.')
+
+if args.book:
+    print(args.book)
+    if not book_store:
+        print('not book_store')
+    else:
+        if book_store in Store_list:
+            print('Changing booking of store ' + str(book_store))
+            Store_list[book_store]['Booker'] = args.book[1]
+            Store_list[book_store]['StartDate'] = args.book[2]
+            Store_list[book_store]['EndDate'] = args.book[3]
+            with open(r'.\store_file.yaml', 'w') as file:
+                document = yaml.dump(Store_list, file, sort_keys=False)
         else:
             print('No store matching that name was found.')
